418a5c92e3fec03aebc9ad287aa36d283e204462
Asher Dawes <asher.dawes@gmail.com>
2018-12-30 16:09:27 -0800

    Correcting start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,4 +7,4 @@ Threads=
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-./nheqminer -v -l "$(PoolHost)":"$(Port)" -u "$(PublicVerusCoinAddress)"."$(WorkerName)" -t "$(Threads)" "$@"
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
